f8380c3839675459840f5584a9ac0ebe912b2ddf
Cameron <camjw119@gmail.com>
2019-12-19 21:31:50 +0000

    made a load of changes but all pretty minor

--- src/frame.js
+++ src/frame.js
@@ -1,52 +1,52 @@
-"use strict";
 
 function Frame() {
-  this.frameScore = 0
+  this.frameScore = 0;
   this.roll = [];
   this.strike = false;
   this.spare = false;
 }
 
-Frame.prototype.getRolls = function() {
-  return this.roll
-}
+Frame.prototype.getRolls = function () {
+  // this is just personal preference but I probably wouldn't bother writing this
+  // sort of method since there are no private variables in Javascript anyway
+  return this.roll;
+};
 
-Frame.prototype.getFrameScore = function() {
-  this.frameScore = this.calcFrameScore()
-  return this.frameScore
-}
+Frame.prototype.getFrameScore = function () {
+  this.frameScore = this.calcFrameScore();
+  return this.frameScore;
+};
 
-Frame.prototype.bowl = function(knockedPins) {
+Frame.prototype.bowl = function (knockedPins) {
   if (knockedPins <= 10 && this.calcFrameScore(knockedPins) <= 10 && this.validateFrameLength()) {
-    this.roll.push(knockedPins)
-    this.isStrike()
-    this.isSpare()
-    return this.getFrameScore()
-  } else {
-    return this.getFrameScore()
+    this.roll.push(knockedPins);
+    this.isStrike();
+    this.isSpare();
   }
-}
 
-Frame.prototype.validateFrameLength = function() {
-  if (this.roll.length >= 2) {
-    return false
-  } else {
-    return true
-  }
-}
+  return this.getFrameScore();
+};
 
-Frame.prototype.calcFrameScore = function(num=0) {
-  return this.roll.reduce((a, b) => a + b, num)
-}
+Frame.prototype.validateFrameLength = function () {
+  return this.roll.length < 2
+};
 
-Frame.prototype.isStrike = function() {
+Frame.prototype.calcFrameScore = function (num = 0) {
+  return this.roll.reduce((a, b) => a + b, num);
+};
+
+Frame.prototype.isStrike = function () {
   if (this.roll[0] === 10) {
     this.strike = true;
   }
-}
+};
 
-Frame.prototype.isSpare = function() {
+Frame.prototype.isSpare = function () {
   if (this.calcFrameScore() === 10) {
     this.spare = true;
   }
+};
+
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+  module.exports = Frame;
 }
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,80 +1,86 @@
-"use strict";
 
 function Scorecard() {
   this.frameNumber = 1;
-  this.frames = { 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[] };
-  this.bonusFrame = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0};
+  this.frames = {
+    1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [],
+  };
+  this.bonusFrame = {
+    1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0,
+  };
   this.bonusScore = 0;
   this.cumulativeScore = 0;
   this.finalFrameBonus = [];
   this.gameOver = false;
 }
 
-Scorecard.prototype.initFrames = function(frame){
+Scorecard.prototype.initFrames = function (frame) {
   this.frames[this.frameNumber] = frame;
   this.frameNumber++;
   this.calcFrameBonus();
-}
+};
 
-Scorecard.prototype.getCumulativeScore = function() {
-  for (var i = 1; i < 11; i++) {
+Scorecard.prototype.getCumulativeScore = function () {
+  for (let i = 1; i < 11; i++) {
     this.cumulativeScore += this.frames[i].frameScore;
   }
-  return this.cumulativeScore
-}
+  return this.cumulativeScore;
+};
 
 // for first 8 frames
-Scorecard.prototype.calcFrameBonus = function() {
-  for (var i = 1; i < 9; i++) {
+Scorecard.prototype.calcFrameBonus = function () {
+  for (let i = 1; i < 9; i++) {
     if (this.frames[i].strike) {
-      this.bonusFrame[i] = (this.frames[i+1].frameScore || 0) + (this.frames[i+2].frameScore || 0)
-    }
-    else if (this.frames[i].spare) {
-      this.bonusFrame[i] = this.frames[i+1][0]
+      this.bonusFrame[i] = (this.frames[i + 1].frameScore || 0) + (this.frames[i + 2].frameScore || 0);
+    } else if (this.frames[i].spare) {
+      this.bonusFrame[i] = this.frames[i + 1][0];
     }
   }
-  this.getPenultimateFrameBonus()
-  return this.bonusFrame
-}
+  this.getPenultimateFrameBonus();
+  return this.bonusFrame;
+};
 
 // for 9th frame
-Scorecard.prototype.getPenultimateFrameBonus = function() {
+Scorecard.prototype.getPenultimateFrameBonus = function () {
   if (this.frames[9].strike) {
-    this.bonusFrame[9] = (this.frames[9].frameScore || 0) + (this.frames[10].frameScore || 0)
+    this.bonusFrame[9] = (this.frames[9].frameScore || 0) + (this.frames[10].frameScore || 0);
   } else if (this.frames[9].spare) {
-    this.bonusFrame[9] = this.frames[9][0]
+    this.bonusFrame[9] = this.frames[9][0];
   }
-}
+};
 
 // for 10th frame
-Scorecard.prototype.getFinalFrameBonus = function(pins) {
-  if (pins > 10){
-    console.log('You cannot knock more than 10 pins')
+Scorecard.prototype.getFinalFrameBonus = function (pins) {
+  if (pins > 10) {
+    console.log('You cannot knock more than 10 pins');
   } else if (this.frames[10].strike || this.frames[10].spare) {
-    if (pins === 10){
-      this.finalFrameBonus.push(pins)
-      this.lastBonus
+    if (pins === 10) {
+      this.finalFrameBonus.push(pins);
+      this.lastBonus;
     } else {
-      this.finalFrameBonus.push(pins)
+      this.finalFrameBonus.push(pins);
     }
   }
-}
+};
 
-Scorecard.prototype.lastBonus = function(pins) {
-  this.finalFrameBonus.push(pins)
-}
+Scorecard.prototype.lastBonus = function (pins) {
+  this.finalFrameBonus.push(pins);
+};
 
-Scorecard.prototype.calcFinalFrameBonus = function() {
+Scorecard.prototype.calcFinalFrameBonus = function () {
   return this.finalFrameBonus.reduce((a, b) => a + b, 0);
-}
+};
 
-Scorecard.prototype.calcBonusScore = function() {
-  for (var i = 1; i < 11; i++){
+Scorecard.prototype.calcBonusScore = function () {
+  for (let i = 1; i < 11; i++) {
     this.bonusScore += this.bonusFrame[i];
   }
   return this.bonusScore;
-}
+};
 
-Scorecard.prototype.getTotalScore = function() {
+Scorecard.prototype.getTotalScore = function () {
   return this.getCumulativeScore() + this.calcBonusScore() + this.calcFinalFrameBonus();
+};
+
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+  module.exports = Scorecard;
 }
